47368390cbf95c2d4e7d095d177f632d72028f44
Pulzze92 <89998310392@mail.ru>
2021-03-10 16:35:08 +0300

    init: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = new Map(bracketsConfig);
+  let array = [];
+		for (let i = 0; i < str.length; i++) {
+			if (str[i] === a.get(array[array.length - 1])) {
+				array.pop();
+			}
+				else { array.push(str[i]);
+				}
+		}
+		return (array.length === 0) ? true : false;
 }
